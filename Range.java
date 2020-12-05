281c8304ea46813f80ee87f413ed40441ec1a63c
Igbathscaller <Igbathscaller@gmail.com>
2020-12-04 21:57:26 -0500

    exception

--- Range.java
+++ Range.java
@@ -27,7 +27,11 @@ public class Range implements IntegerSequence{
   //This will return the current value, it will also increase current value by 1. 
   //e.g.  if current is 5. This will make current 6, and return 5. 
   public int next(){
+    if (hasNext()){
     return current++;
+    }
+    else{
+        throw new NoSuchElementException("End of Sequence");
+    }
   }
-  
 }

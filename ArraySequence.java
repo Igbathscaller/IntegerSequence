c80856efe277c7a48ce92c64a5ee7c739b04f2f4
Igbathscaller <Igbathscaller@gmail.com>
2020-12-08 12:43:36 -0500

    array seq interface functions

---
+++ ArraySequence.java
@@ -0,0 +1,37 @@
+import java.util.NoSuchElementException;
+public class ArraySequence implements IntegerSequence{
+    private int currentIndex;
+    private int[] data;
+
+    /*Construct the sequence by copying values from the other array into the data array*/
+    public ArraySequence(int[] other){
+        currentIndex = 0;
+        data = new int[other.length];
+        for(int i = 0; i < data.length; i++){
+            data[i]=other[i];
+        }
+    }
+    //does the sequence have more elements? 
+    public boolean hasNext(){
+        return currentIndex<data.length;
+    }
+
+    //return the current value in the sequence and advances to the next element
+    public int next(){
+        if (hasNext()){
+            return data[currentIndex++];
+            }
+        else{
+            throw new NoSuchElementException("End of Sequence");
+        }
+    }
+
+    //returns the total length of the sequence
+    public int length(){
+        return data.length;
+    }
+    //start over from the start of the sequence  
+    public void reset(){
+        currentIndex = 0;
+    }
+}
